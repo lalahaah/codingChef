6090f84727a1638e032bdaf9ba8576f251fd66b6
LalaHaha <120492982+lalahaah@users.noreply.github.com>
2023-11-28 22:27:17 +0900

    yummyChat

---
+++ YummyChat/android/app/src/main/kotlin/com/example/chat_codingchef/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_codingchef
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ YummyChat/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:chat_codingchef/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting app',
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: LoginSignupScreen(),
+    );
+  }
+}
